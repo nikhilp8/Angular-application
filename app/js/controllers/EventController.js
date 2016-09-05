7d8af4b459232be1f8287f072c43f57499af3e26
nikhil.ponugoti <nikhil.ponugoti@gmail.com>
2016-09-05 00:18:05 -0500

    Add EventController with event data

---
+++ app/js/controllers/EventController.js
@@ -0,0 +1,10 @@
+'use strict';
+
+eventsApp.controller('EventController', function EventController($scope) {
+	$scope.Event = {
+			name: 'AngularJS',
+			date: 'Sept-4-2016',
+			description: 'Welcome to AngularJS Event',
+			src: 'img/angularjs-logo.png'
+			}
+	});
