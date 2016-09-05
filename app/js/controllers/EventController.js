4574b912fee96accef8c7b75d9b54d2e90b6cbe7
nikhil.ponugoti <nikhil.ponugoti@gmail.com>
2016-09-05 01:34:45 -0500

    Add other sessions in Event Details uisng ng-repeat

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -5,6 +5,29 @@ eventsApp.controller('EventController', function EventController($scope) {
 			name: 'AngularJS',
 			date: 'Sept-4-2016',
 			description: 'Welcome to AngularJS Event',
-			src: 'img/angularjs-logo.png'
+			src: 'img/angularjs-logo.png',
+			location: {
+				address: '4724 N 100th st',
+				city: 'wauwatosa',
+				state: 'WI'
+			},
+			Sessions: [
+			{
+				course: 'Angular 1.5',
+				author: 'Joe Eames',
+				duration: '5 hr'
+			},
+			{	
+				course: 'Bootstrap 3',
+				author: 'Shawn Wilderman',
+				duration: '2 hr'
+
+			},
+			{	
+				course: 'Advanced JS',
+				author: 'Scot allen',
+				duration: '4 hr'
+
+			}]
 			}
 	});
