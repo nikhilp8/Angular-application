c8decccdbb0c90e40260b8f9f444092d2195b954
nikhil.ponugoti <nikhil.ponugoti@gmail.com>
2016-09-06 13:56:52 -0500

    Add changes to EditEventDetails,remove previous form complexities and add simple form layout

--- app/js/controllers/EditEventController.js
+++ app/js/controllers/EditEventController.js
@@ -4,4 +4,10 @@ eventsApp.controller('EditEventController', function EditEventController($scope)
 	$scope.cancel = function(){
 		window.location = '/EventDetails.html';
 	}
+	$scope.save = function(event, newEventForm){
+		if(newEventform,&valid)
+		{
+			window.alert(event.name+' new event saved');
+		}
+	}
 });
