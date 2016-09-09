d428d79497d35573f70ca081b096e0f5a93836ef
nikhil.ponugoti <nikhil.ponugoti@gmail.com>
2016-09-09 08:06:29 -0500

    Fixed the custom service bug

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,40 +1,9 @@
 'use strict';
 
-eventsApp.controller('EventController', function EventController($scope) {
+eventsApp.controller('EventController', function EventController($scope, EventData) {
 	$scope.sortOrder= '-upVote';
 
-	$scope.Event = {
-			name: 'AngularJS',
-			date: 'Sept-4-2016',
-			description: 'Welcome to AngularJS Event',
-			src: 'img/angularjs-logo.png',
-			location: {
-				address: '4724 N 100th st',
-				city: 'wauwatosa',
-				state: 'WI'
-			},
-			Sessions: [
-			{
-				course: 'Angular 1.5',
-				author: 'Joe Eames',
-				duration: 1,
-				upVote: 0
-			},
-			{	
-				course: 'Bootstrap 3',
-				author: 'Shawn Wilderman',
-				duration: 2,
-				upVote: 0
-
-			},
-			{	
-				course: 'Advanced JS',
-				author: 'Scot allen',
-				duration: 4,
-				upVote: 0
-
-			}]
-			}
+	$scope.Event = EventData.event;
 	$scope.upVote = function(x) {
 		x.upVote++;
 	}
